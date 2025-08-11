d4f5bbef0f3abb8696a3799e63f657606b9d1569
Patrik <22777727+fedorish@users.noreply.github.com>
2025-08-11 18:06:11 +0200

    fix: formatting and testing to add container for toolbar

--- ios/ImageViewer.swift/ImageCarouselViewController.swift
+++ ios/ImageViewer.swift/ImageCarouselViewController.swift
@@ -2,11 +2,13 @@ import UIKit
 
 public protocol ImageDataSource: AnyObject {
     func numberOfImages() -> Int
-    func imageItem(at index:Int) -> ImageItem
+    func imageItem(at index: Int) -> ImageItem
 }
 
-public class ImageCarouselViewController:UIPageViewController, ImageViewerTransitionViewControllerConvertible {
-    
+public class ImageCarouselViewController: UIPageViewController,
+    ImageViewerTransitionViewControllerConvertible
+{
+
     unowned var initialSourceView: UIImageView?
     var sourceView: UIImageView? {
         guard let vc = viewControllers?.first as? ImageViewerController else {
@@ -14,64 +16,76 @@ public class ImageCarouselViewController:UIPageViewController, ImageViewerTransi
         }
         return initialIndex == vc.index ? initialSourceView : nil
     }
-    
+
     var targetView: UIImageView? {
         guard let vc = viewControllers?.first as? ImageViewerController else {
             return nil
         }
         return vc.imageView
     }
-    
-    weak var imageDatasource:ImageDataSource?
-    let imageLoader:ImageLoader
- 
+
+    weak var imageDatasource: ImageDataSource?
+    let imageLoader: ImageLoader
+
     var initialIndex = 0
-    
-    var theme:ImageViewerTheme = .light {
+
+    var theme: ImageViewerTheme = .light {
         didSet {
             navItem.leftBarButtonItem?.tintColor = theme.tintColor
             backgroundView?.backgroundColor = theme.color
+            toolbar.tintColor = theme.tintColor
+
         }
     }
-    
+
     var imageContentMode: UIView.ContentMode = .scaleAspectFill
-    var options:[ImageViewerOption] = []
-    
-    private var onRightNavBarTapped:((Int) -> Void)?
-    
-    private(set) lazy var navBar:UINavigationBar = {
+    var options: [ImageViewerOption] = []
+
+    private var onRightNavBarTapped: ((Int) -> Void)?
+
+    private(set) lazy var navBar: UINavigationBar = {
         let _navBar = UINavigationBar(frame: .zero)
         _navBar.isTranslucent = true
         _navBar.setBackgroundImage(UIImage(), for: .default)
         _navBar.shadowImage = UIImage()
         return _navBar
     }()
-    
-    private(set) lazy var backgroundView:UIView? = {
+
+    internal(set) lazy var toolbar: UIToolbar = {
+        let _toolbar = UIToolbar(frame: .zero)
+        _toolbar.isTranslucent = true
+        _toolbar.setBackgroundImage(UIImage(), forToolbarPosition: .any, barMetrics: .default)
+        _toolbar.setShadowImage(UIImage(), forToolbarPosition: .any)
+        _toolbar.alpha = 0.0  // Start hidden like the nav bar
+        return _toolbar
+    }()
+
+    private(set) lazy var backgroundView: UIView? = {
         let _v = UIView()
         _v.backgroundColor = theme.color
         _v.alpha = 1.0
         return _v
     }()
-    
+
     private(set) lazy var navItem = UINavigationItem()
-    
+
     private let imageViewerPresentationDelegate: ImageViewerTransitionPresentationManager
-    
+
     public init(
-        sourceView:UIImageView,
+        sourceView: UIImageView,
         imageDataSource: ImageDataSource?,
         imageLoader: ImageLoader,
-        options:[ImageViewerOption] = [],
-        initialIndex:Int = 0) {
-        
+        options: [ImageViewerOption] = [],
+        initialIndex: Int = 0
+    ) {
+
         self.initialSourceView = sourceView
         self.initialIndex = initialIndex
         self.options = options
         self.imageDatasource = imageDataSource
         self.imageLoader = imageLoader
         let pageOptions = [UIPageViewController.OptionsKey.interPageSpacing: 20]
-        
+
         var _imageContentMode = imageContentMode
         options.forEach {
             switch $0 {
@@ -82,22 +96,23 @@ public class ImageCarouselViewController:UIPageViewController, ImageViewerTransi
             }
         }
         imageContentMode = _imageContentMode
-        
-        self.imageViewerPresentationDelegate = ImageViewerTransitionPresentationManager(imageContentMode: imageContentMode)
+
+        self.imageViewerPresentationDelegate = ImageViewerTransitionPresentationManager(
+            imageContentMode: imageContentMode)
         super.init(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal,
             options: pageOptions)
-        
+
         transitioningDelegate = imageViewerPresentationDelegate
         modalPresentationStyle = .custom
         modalPresentationCapturesStatusBarAppearance = true
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     private func addNavBar() {
         // Add Navigation Bar
         let closeBarButton = UIBarButtonItem(
@@ -105,60 +120,100 @@ public class ImageCarouselViewController:UIPageViewController, ImageViewerTransi
             style: .plain,
             target: self,
             action: #selector(dismiss(_:)))
-        
+
         navItem.leftBarButtonItem = closeBarButton
         navItem.leftBarButtonItem?.tintColor = theme.tintColor
         navBar.alpha = 0.0
         navBar.items = [navItem]
         navBar.insert(to: view)
     }
-    
+
+    private func addToolbar() {
+        view.addSubview(toolbar)
+        toolbar.translatesAutoresizingMaskIntoConstraints = false
+        toolbar.backgroundColor = .clear
+        
+
+        NSLayoutConstraint.activate([
+            toolbar.leftAnchor.constraint(equalTo: view.leftAnchor),
+            toolbar.rightAnchor.constraint(equalTo: view.rightAnchor),
+            toolbar.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+
+    // private func setupToolbar() {
+    //     let flexSpace = UIBarButtonItem(
+    //         barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+
+    //     let shareButton = UIBarButtonItem(
+    //         barButtonSystemItem: .action,
+    //         target: self,
+    //         action: #selector(shareTapped))
+
+    //     let bookmarkButton = UIBarButtonItem(
+    //         image: UIImage(systemName: "bookmark"),
+    //         style: .plain,
+    //         target: self,
+    //         action: #selector(bookmarkTapped))
+
+    //     let infoButton = UIBarButtonItem(
+    //         image: UIImage(systemName: "info.circle"),
+    //         style: .plain,
+    //         target: self,
+    //         action: #selector(infoTapped))
+
+    //     toolbar.items = [bookmarkButton, flexSpace, shareButton, flexSpace, infoButton]
+    //     toolbar.tintColor = theme.tintColor
+    // }
+
     private func addBackgroundView() {
         guard let backgroundView = backgroundView else { return }
         view.addSubview(backgroundView)
         backgroundView.bindFrameToSuperview()
         view.sendSubviewToBack(backgroundView)
     }
-    
+
     private func applyOptions() {
-        
+
         options.forEach {
             switch $0 {
-                case .theme(let theme):
-                    self.theme = theme
-                case .contentMode(let contentMode):
-                    self.imageContentMode = contentMode
-                case .closeIcon(let icon):
-                    navItem.leftBarButtonItem?.image = icon
-                case .rightNavItemTitle(let title, let onTap):
-                    navItem.rightBarButtonItem = UIBarButtonItem(
-                        title: title,
-                        style: .plain,
-                        target: self,
-                        action: #selector(diTapRightNavBarItem(_:)))
-                    onRightNavBarTapped = onTap
-                case .rightNavItemIcon(let icon, let onTap):
-                    navItem.rightBarButtonItem = UIBarButtonItem(
-                        image: icon,
-                        style: .plain,
-                        target: self,
-                        action: #selector(diTapRightNavBarItem(_:)))
-                    onRightNavBarTapped = onTap
+            case .theme(let theme):
+                self.theme = theme
+            case .contentMode(let contentMode):
+                self.imageContentMode = contentMode
+            case .closeIcon(let icon):
+                navItem.leftBarButtonItem?.image = icon
+            case .rightNavItemTitle(let title, let onTap):
+                navItem.rightBarButtonItem = UIBarButtonItem(
+                    title: title,
+                    style: .plain,
+                    target: self,
+                    action: #selector(diTapRightNavBarItem(_:)))
+                onRightNavBarTapped = onTap
+            case .rightNavItemIcon(let icon, let onTap):
+                navItem.rightBarButtonItem = UIBarButtonItem(
+                    image: icon,
+                    style: .plain,
+                    target: self,
+                    action: #selector(diTapRightNavBarItem(_:)))
+                onRightNavBarTapped = onTap
             }
         }
     }
-    
+
     override public func viewDidLoad() {
         super.viewDidLoad()
-        
+
         addBackgroundView()
         addNavBar()
+        addToolbar()
+        // setupToolbar()
         applyOptions()
-        
+
         dataSource = self
 
         if let imageDatasource = imageDatasource {
-            let initialVC:ImageViewerController = .init(
+            let initialVC: ImageViewerController = .init(
                 index: initialIndex,
                 imageItem: imageDatasource.imageItem(at: initialIndex),
                 imageLoader: imageLoader)
@@ -167,22 +222,22 @@ public class ImageCarouselViewController:UIPageViewController, ImageViewerTransi
     }
 
     @objc
-    private func dismiss(_ sender:UIBarButtonItem) {
+    private func dismiss(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
     }
-    
+
     deinit {
         initialSourceView?.alpha = 1.0
     }
-    
+
     @objc
-    func diTapRightNavBarItem(_ sender:UIBarButtonItem) {
+    func diTapRightNavBarItem(_ sender: UIBarButtonItem) {
         guard let onTap = onRightNavBarTapped,
             let _firstVC = viewControllers?.first as? ImageViewerController
-            else { return }
+        else { return }
         onTap(_firstVC.index)
     }
-    
+
     override public var preferredStatusBarStyle: UIStatusBarStyle {
         if theme == .dark {
             return .lightContent
@@ -191,30 +246,32 @@ public class ImageCarouselViewController:UIPageViewController, ImageViewerTransi
     }
 }
 
-extension ImageCarouselViewController:UIPageViewControllerDataSource {
+extension ImageCarouselViewController: UIPageViewControllerDataSource {
     public func pageViewController(
         _ pageViewController: UIPageViewController,
-        viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        
+        viewControllerBefore viewController: UIViewController
+    ) -> UIViewController? {
+
         guard let vc = viewController as? ImageViewerController else { return nil }
         guard let imageDatasource = imageDatasource else { return nil }
         guard vc.index > 0 else { return nil }
- 
+
         let newIndex = vc.index - 1
         return ImageViewerController.init(
             index: newIndex,
-            imageItem:  imageDatasource.imageItem(at: newIndex),
+            imageItem: imageDatasource.imageItem(at: newIndex),
             imageLoader: vc.imageLoader)
     }
-    
+
     public func pageViewController(
         _ pageViewController: UIPageViewController,
-        viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        
+        viewControllerAfter viewController: UIViewController
+    ) -> UIViewController? {
+
         guard let vc = viewController as? ImageViewerController else { return nil }
         guard let imageDatasource = imageDatasource else { return nil }
         guard vc.index <= (imageDatasource.numberOfImages() - 2) else { return nil }
-        
+
         let newIndex = vc.index + 1
         return ImageViewerController.init(
             index: newIndex,
--- ios/ImageViewer.swift/UINavigationBar_Extensions.swift
+++ ios/ImageViewer.swift/UINavigationBar_Extensions.swift
@@ -1,13 +1,13 @@
 import UIKit
 
 extension UINavigationBar {
-    
+
     func insert(to view: UIView) {
         view.addSubview(self)
         translatesAutoresizingMaskIntoConstraints = false
         leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        
+
         if #available(iOS 11.0, *) {
             topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
                 .isActive = true
